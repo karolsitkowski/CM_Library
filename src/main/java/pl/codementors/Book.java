d54f38fe5acfa30fa1f7c814cec54b800d3da93b
karolsitkowski <karol.sitkowski@gmail.com>
2017-06-01 17:53:25 +0200

    Zadanie3

---
+++ src/main/java/pl/codementors/Book.java
@@ -0,0 +1,35 @@
+package pl.codementors;
+
+/**
+ * Created by sit0 on 01.06.17.
+ */
+public class Book {
+
+    private String title;
+    private String author;
+    private String releaseYear;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+}
