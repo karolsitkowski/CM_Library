b7400e54921a1ddfe30d6b5b94cf124d2539641c
karolsitkowski <karol.sitkowski@gmail.com>
2017-06-01 17:42:16 +0200

    Zadanie1 done

---
+++ src/main/java/pl/codementors/MainApp.java
@@ -0,0 +1,10 @@
+package pl.codementors;
+
+/**
+ * Created by sit0 on 01.06.17.
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
