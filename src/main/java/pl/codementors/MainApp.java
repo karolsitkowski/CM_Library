67c2f6b3f1fe9de72905890eede0a506a1ffec21
karolsitkowski <karol.sitkowski@gmail.com>
2017-06-01 19:15:35 +0200

    Zadanie5

--- src/main/java/pl/codementors/MainApp.java
+++ src/main/java/pl/codementors/MainApp.java
@@ -7,17 +7,55 @@ import java.util.Scanner;
  */
 public class MainApp {
     public static void main(String[] args) {
+
+        final int INITIAL_SIZE = 10;
+
         Scanner inputScanner = new Scanner(System.in);
-        Book myBook = new Book();
 
-        System.out.println("Podaj tytuł książki");
-        myBook.setTitle(inputScanner.nextLine());
-        System.out.println("Podaj autora:");
-        myBook.setAuthor(inputScanner.nextLine());
-        System.out.println("Podaj rok wydania");
-        myBook.setReleaseYear(inputScanner.nextInt());
+        Book[] bookArr = new Book[10];
 
-        System.out.println("Tytuł książki: " + myBook.getTitle() +"\nAutor: " + myBook.getAuthor() + "\nRok wydania: " + myBook.getReleaseYear());
+        System.out.println("MENU:\nWpisz D -> Dodaj Ksiązkę\nWpisz X -> Wyjdz z programu\nWpisz numer operacji");
+        boolean runner = true;
+        while(runner){
+
+
+            switch (inputScanner.nextLine()){
+                case "D":{
+                    System.out.println("---Wpisujesz książkę---");
+                    Book myBook = new Book();
+                    System.out.println("Podaj tytuł książki:");
+                    myBook.setTitle(inputScanner.nextLine());
+                    System.out.println("Podaj autora:");
+                    myBook.setAuthor(inputScanner.nextLine());
+                    System.out.println("Podaj rok wydania");
+                    myBook.setReleaseYear(inputScanner.nextInt());
+                    System.out.println("Tytuł książki: " + myBook.getTitle() +"\nAutor: "
+                            + myBook.getAuthor() + "\nRok wydania: "
+                            + myBook.getReleaseYear() + "\nPodaj miejsce na półce(od 1 do 10)");
+                    //TODO dodaj walidacje
+                    bookArr[inputScanner.nextInt()-1] = myBook;
+                    break;
+                }
+                case "X":{
+                    runner = false;
+                    break;
+                }
+
+
+                default: {
+                    System.out.println("MENU:\nWpisz D -> Dodaj Ksiązkę\nWpisz X -> Wyjdz z programu\nWpisz numer operacji");
+                }
+
+            }
+        }
+
+
+
+
+
+
+        System.out.println("Test:" + bookArr[0].getTitle());
+        System.out.println("Test:" + bookArr[1].getTitle());
 
 
 
