282e4553278be8b2b592c97e4e45b26aa7b42c75
karolsitkowski <karol.sitkowski@gmail.com>
2017-06-02 15:42:06 +0200

    Zad10 done

---
+++ src/main/java/pl/codementors/BookStand.java
@@ -0,0 +1,25 @@
+package pl.codementors;
+
+/**
+ * Created by sit0 on 02.06.17.
+ */
+public class BookStand {
+
+    private Shelf[] bookStand;
+    private int bookStandSize;
+
+    public BookStand(int size) {
+        this.bookStand = new Shelf[size];
+    }
+
+    public BookStand(){
+    }
+
+    public Shelf getShelfFromBookstand(int index) {
+        return bookStand[index];
+    }
+
+    public void addShelveToBookStand(Shelf shelf,int index) {
+        this.bookStand[index] = shelf;
+    }
+}
