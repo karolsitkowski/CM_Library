c16802d5c2643b3ed4b5761cdefe509e69699449
karolsitkowski <karol.sitkowski@gmail.com>
2017-06-15 16:03:49 +0200

    S2Z10: print method overrided in comic and magazine class

--- src/main/java/pl/codementors/ComicBook.java
+++ src/main/java/pl/codementors/ComicBook.java
@@ -25,4 +25,18 @@ public class ComicBook extends Book {
     public void setPublishingSeries(String publishingSeries) {
         this.publishingSeries = publishingSeries;
     }
+    @Override
+    public void print() {
+
+        System.out.println("Typ: " + getClass().getSimpleName());
+        System.out.println("Tytuł: " + getTitle());
+        System.out.println("Autorzy:");
+        for (int i = 0; i < getAuthors().length; i++) {
+            System.out.println("Imię i nazwisko autora " + (i + 1) + ": " + getAuthors()[i].getName() + " " + getAuthors()[i].getSurName());
+            System.out.println("Pseudonim artystyczny: " + getAuthors()[i].getStageName());
+        }
+        System.out.println("Data wydania: " + releaseDate);
+        System.out.println("Wydawnictwo: " + publishingSeries);
+        System.out.println("-----------------");
+    }
 }
--- src/main/java/pl/codementors/Magazine.java
+++ src/main/java/pl/codementors/Magazine.java
@@ -15,4 +15,18 @@ public class Magazine extends Book {
     public void setReleaseYear(String releaseYear) {
         this.releaseDate = releaseDate;
     }
+
+    @Override
+    public void print() {
+
+        System.out.println("Typ: " + getClass().getSimpleName());
+        System.out.println("Tytuł: " + getTitle());
+        System.out.println("Autorzy:");
+        for (int i = 0; i < getAuthors().length; i++) {
+            System.out.println("Imię i nazwisko autora " + (i + 1) + ": " + getAuthors()[i].getName() + " " + getAuthors()[i].getSurName());
+            System.out.println("Pseudonim artystyczny: " + getAuthors()[i].getStageName());
+        }
+        System.out.println("Data wydania: " + releaseDate);
+        System.out.println("-----------------");
+    }
 }
